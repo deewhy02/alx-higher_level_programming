db81e4d0c2125badb7dc9525a43c4d3ca8bb4cd4
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-07-19 07:06:08 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Define a Square"""
+
+
 class Square:
+    """ Represent a Square."""
     def __init__(self, size):
+        """ Initialize a new Square.
+        Args:
+            size (int): The size of a new Square.
+        """
         self.__size = size
