d27bfc5a9b930ffe5a700f36b2283eac2fe06bcc
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-07-20 01:39:29 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define a Square"""
+
+
+class Square:
+    """ Represent a Square."""
+    def __init__(self, size=0):
+        """ Initialize a new Square:
+        Args:
+            size (int): The size of a new Square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ returns the current square of the area."""
+        return (self.__size * self.__size)
