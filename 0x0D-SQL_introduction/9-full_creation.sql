a0197517d7c61de1a0f82d1296ee61d18869cad5
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-09-06 13:41:56 -0700

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- create a new table and fill with data
 
-CREATE TABLE IF NOT EXISTS second_table (id = INT, name = VARCHAR(256), score = INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, 'john', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
