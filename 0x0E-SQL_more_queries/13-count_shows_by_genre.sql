8940c8e66be696af4516bed05159e8914b845d72
deewhy02 <101807496+deewhy02@users.noreply.github.com>
2022-09-08 05:33:42 -0700

    tasks

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- wget https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
--- run this first to import a SQL dump --
+-- run this first to import a SQL dump -->
 --      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
 --      import dumb table into hbtn_0d_tvshows
 --
@@ -10,10 +10,10 @@
 -- can use only one SELECT statement
 -- the database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name AS genre,
-         COUNT(*) AS 'number_shows'
-    FROM tv_genres
-  INNER JOIN tv_show_genres
-    ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-ORDER BY number_shows DESC;
+SELECT tv_genres.name AS genre, 
+	   COUNT(*) AS 'number_shows'
+   FROM tv_genres
+		INNER JOIN tv_show_genres
+        ON tv_genres.id = tv_show_genres.genre_id
+   GROUP BY genre
+   ORDER BY number_shows DESC;
